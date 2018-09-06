f5074ce488abde21f1ff65d065bbbf7aadb7ee82
Mattias Asplund <mattias@mattiasasplund.se>
2018-09-06 23:02:32 +0200

    MSSQL exportdefinion.sql outputs schema, length, precision and scale :-)

--- mssql/exportdefinition.sql
+++ mssql/exportdefinition.sql
@@ -3,28 +3,35 @@ set nocount on
 select db.content --, sort, tbl, innerSort
 from
 (
-    select 0 sort, '' tbl, 0 innerSort, 0 col, '<?xml version="1.0"?>' content
+    select 0 sort, '' [schema], '' tbl, 0 innerSort, 0 col, '<?xml version="1.0"?>' content
     union
-    select 1, '', 0, 0, CONCAT('<Database name="', db_name(), '">')
+    select 1, '', '', 0, 0, CONCAT('<Database name="', db_name(), '">')
     union
-    select 2, t.name, 0, 0, CONCAT('<Table name="', t.name, '">')
+    select 2, s.name, t.name, 0, 0, CONCAT('<Table name="', t.name, '" schema="', s.name, '">')
     from
         sys.tables t
+    inner join
+        sys.schemas s on s.schema_id=t.schema_id    
     union
-    select 3, t.name, 0, 0, '<Fields>'
+    select 3, s.name, t.name, 0, 0, '<Fields>'
     from
         sys.tables t
+    inner JOIN
+        sys.schemas s on s.schema_id = t.schema_id
     union
 
-    select top 100 percent 4, t.name, 0, c.ordinal_position, CONCAT('<Field name="', co.name, '" type="', 
+    select top 100 percent 4, s.name, t.name, 0, c.ordinal_position /*, c.character_maximum_length, c.numeric_precision, c.numeric_scale */, CONCAT('<Field name="', co.name, '" type="', 
         case
-            when ty.name='varchar' then CONCAT(ty.name, '(255)')
+            when ty.name='varchar' then CONCAT(ty.name, '(' + CAST(c.character_maximum_length AS NVARCHAR(10)) + ')')
+            when ty.name='bit' then 'bit'
+            when ty.name='numeric' then 'decimal'
+            when ty.name='decimal' then 'decimal'
             when ty.name='int' then 'int'
             when ty.name='date' then 'date'
             when ty.name='float' then 'float'
             when ty.name='time' then 'time'
-            when ty.name='char' then CONCAT('char', '(66)')
-        end, '" nullable="', lower(c.is_nullable), '"/>')
+            when ty.name='char' then 'char'
+        end, CONCAT('" nullable="', lower(c.is_nullable), '" length="', CAST(c.character_maximum_length AS NVARCHAR(10)), '"', ' precision="', CAST(c.numeric_precision AS NVARCHAR(10)), '" scale="', CAST(c.numeric_scale AS NVARCHAR(10)), '"/>'))
     from
         sys.tables t
     inner join
@@ -33,21 +40,27 @@ from
         sys.columns co on co.name = c.column_name
     inner join
         sys.types ty on ty.system_type_id=co.system_type_id
+	inner join
+		sys.schemas s on s.schema_id=t.schema_id
     order by
         c.ordinal_position
 
     union
 
-    select 5, t.name, 0, 0, '</Fields>'
+    select 5, s.name, t.name, 0, 0, '</Fields>'
     from
         sys.tables t
+    inner JOIN
+        sys.schemas s on s.schema_id = t.schema_id
     union
-    select 6, t.name, 0, 0, '<PrimaryKey>'
+    select 6, s.name, t.name, 0, 0, '<PrimaryKey>'
     from
         sys.tables t
+    inner JOIN
+        sys.schemas s on s.schema_id=t.schema_id
     union
 
-    select 7, t.name, 0, ku.ordinal_position, CONCAT('<Field name="', ku.column_name, '" pos="', CAST(ku.ordinal_position AS char(1)), '"/>')
+    select 7, s.name, t.name, 0, ku.ordinal_position, CONCAT('<Field name="', ku.column_name, '" pos="', CAST(ku.ordinal_position AS char(1)), '"/>')
     from
         sys.tables t
     inner join
@@ -57,33 +70,41 @@ from
             on tc.constraint_type = 'PRIMARY KEY'
             and tc.constraint_name = ku.constraint_name
             and ku.table_name=t.name
+    inner JOIN
+        sys.schemas s on s.schema_id = t.schema_id
 
     union
 
-    select 8, t.name, 0, 0, '</PrimaryKey>'
+    select 8, s.name, t.name, 0, 0, '</PrimaryKey>'
     from
         sys.tables t
+    inner JOIN
+        sys.schemas s on s.schema_id = t.schema_id
 
     union
 
-    select 9, t.name, 0, 0, '<Indexes>'
+    select 9, s.name, t.name, 0, 0, '<Indexes>'
     from
         sys.tables t
+    inner JOIN
+        sys.schemas s on s.schema_id = t.schema_id
 
     union
 
-    select top 100 percent 10, t.name, (ROW_NUMBER() OVER (PARTITION BY t.name ORDER BY t.name)) * 10, 0, CONCAT('<Index name="', i.name, '">')
+    select top 100 percent 10, s.name, t.name, (ROW_NUMBER() OVER (PARTITION BY t.name ORDER BY t.name)) * 10, 0, CONCAT('<Index name="', i.name, '">')
     from
         sys.tables t
     inner join
         sys.indexes i on i.object_id=t.object_id
+    inner JOIN
+        sys.schemas s on s.schema_id = t.schema_id
     where
         i.is_primary_key = 0
 	order by t.name
 
     union
 
-    select top 100 percent 10, object_name(o.object_id), (ROW_NUMBER() OVER (PARTITION BY i.object_id ORDER BY i.object_id)) + (ROW_NUMBER() OVER (PARTITION BY object_name(o.object_id) ORDER BY object_name(o.object_id))) * 10, ic.key_ordinal, CONCAT('<Field name="', co.[name], '" pos="', CAST(ic.key_ordinal AS char(1)), '"/>')
+    select top 100 percent 10, 'x', object_name(o.object_id), (ROW_NUMBER() OVER (PARTITION BY i.object_id ORDER BY i.object_id)) + (ROW_NUMBER() OVER (PARTITION BY object_name(o.object_id) ORDER BY object_name(o.object_id))) * 10, ic.key_ordinal, CONCAT('<Field name="', co.[name], '" pos="', CAST(ic.key_ordinal AS char(1)), '"/>')
     from
         sys.indexes i 
     inner join 
@@ -101,29 +122,33 @@ from
 
     union
 
-    select top 100 percent 10, t.name, (ROW_NUMBER() OVER (PARTITION BY t.name ORDER BY t.name)) * 10 + 9, 0, '</Index>'
+    select top 100 percent 10, s.name, t.name, (ROW_NUMBER() OVER (PARTITION BY t.name ORDER BY t.name)) * 10 + 9, 0, '</Index>'
     from
         sys.tables t
     inner join
         sys.indexes i on i.object_id=t.object_id
+    inner JOIN
+        sys.schemas s on s.schema_id = t.schema_id
     where
         i.is_primary_key = 0
 	order by t.name
 
     union
 
-    select 13, t.name, 0, 0, '</Indexes>'
+    select 13, s.name, t.name, 0, 0, '</Indexes>'
     from
         sys.tables t
+    inner JOIN
+        sys.schemas s on s.schema_id = t.schema_id
 
     union
 
-    select 14, t.name, 0, 0, '</Table>'
+    select 14, s.name, t.name, 0, 0, '</Table>'
     from
         sys.tables t
+    inner JOIN
+        sys.schemas s on s.schema_id = t.schema_id
     union
-    select 999, 'xxx', 0, 0, '</Database>'
+    select 999, '', 'xxx', 0, 0, '</Database>'
 ) db
-order by db.tbl, db.sort, db.innerSort, db.col
-
-
+order by db.[schema], db.tbl, db.sort, db.innerSort, db.col
