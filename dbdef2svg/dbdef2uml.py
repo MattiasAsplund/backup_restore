c2c493b5a61e27180e3fde32cd75fa44e22c1197
Mattias Asplund <mattias@mattiasasplund.se>
2019-09-16 21:31:13 +0200

    Filter on Schema and/or Schema.Table when generating ER diagrams

--- dbdef2svg/dbdef2uml.py
+++ dbdef2svg/dbdef2uml.py
@@ -3,6 +3,26 @@ import xml.etree.ElementTree as ET
 import shutil
 import os
 
+def IncludeSchema(schema):
+    if len(sys.argv) < 3:
+        return True
+    pos = 3
+    while pos < len(sys.argv):
+        if sys.argv[pos] == schema:
+            return True
+        pos += 1
+    return False
+
+def IncludeSchemaTable(schematable):
+    if len(sys.argv) < 3:
+        return True
+    pos = 3
+    while pos < len(sys.argv):
+        if sys.argv[pos] == schematable:
+            return True
+        pos += 1
+    return False
+
 tree = ET.parse(sys.argv[1])
 root = tree.getroot()
 outputPath = sys.argv[2]
@@ -17,30 +37,33 @@ for table in root.findall('Table'):
     name = table.get("name")
     fullName = schema + '.' + name
 
-    f.write('entity ' + fullName + ' {\n')
+    if IncludeSchema(schema) or IncludeSchemaTable(fullName):
+        f.write('entity ' + fullName + ' {\n')
 
-    for pkey in table.findall("PrimaryKey/Field"):
-        f.write('  + ' + pkey.get('name') + ' [PK]\n')
+        for pkey in table.findall("PrimaryKey/Field"):
+            f.write('  + ' + pkey.get('name') + ' [PK]\n')
 
-    f.write('  --\n')
+        f.write('  --\n')
 
-    for fkey in table.findall("Fields/Field"):
-        found = table.find("PrimaryKey/Field[@name='" + fkey.get('name') + "']")
-        if found is None:
-            f.write('  ' + fkey.get('name') + '\n')
+        for fkey in table.findall("Fields/Field"):
+            found = table.find("PrimaryKey/Field[@name='" + fkey.get('name') + "']")
+            if found is None:
+                f.write('  ' + fkey.get('name') + '\n')
 
-    f.write('}\n')
+        f.write('}\n')
 
 for table in root.findall('Table'):
     schema = table.get("schema")
     name = table.get("name")
     fullName = schema + '.' + name
 
-    for fkey in table.findall("ForeignKeys/ForeignKey"):
-        fkSchema = fkey.get("referencedSchema")
-        fkTable = fkey.get("referencedTable")
-        fkFullName = fkSchema + '.' + fkTable
-        f.write(fullName + ' }-- ' + fkFullName + '\n')
+    if IncludeSchema(schema) or IncludeSchemaTable(fullName):
+        for fkey in table.findall("ForeignKeys/ForeignKey"):
+            fkSchema = fkey.get("referencedSchema")
+            fkTable = fkey.get("referencedTable")
+            fkFullName = fkSchema + '.' + fkTable
+            if IncludeSchema(fkSchema) or IncludeSchemaTable(fkFullName):
+                f.write(fullName + ' }-- ' + fkFullName + '\n')
 
 f.write('@enduml\n')
 f.close()
